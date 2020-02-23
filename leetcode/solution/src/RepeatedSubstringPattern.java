e29991453c90c42dc0723a8a56ecbe90d305de89
Wentian Li <kerbo@fb.com>
2020-02-23 13:01:55 -0800

    fd

--- leetcode/solution/src/RepeatedSubstringPattern.java
+++ leetcode/solution/src/RepeatedSubstringPattern.java
@@ -5,15 +5,20 @@ public class RepeatedSubstringPattern {
      */
     public boolean repeatedSubstringPattern(String str) {
         int l = str.length();
+        boolean flag = true;
         for (int i = l / 2; i >= 1; i--) {
             if (l % i == 0) {
                 int m = l / i;
+                if (m % 2 == 0 && !flag) {
+                    continue;
+                }
                 String subS = str.substring(0, i);
                 StringBuilder sb = new StringBuilder();
                 for (int j = 0; j < m; j++) {
                     sb.append(subS);
                 }
                 if (sb.toString().equals(str)) return true;
+                flag = false;
             }
         }
         return false;
